13b2477fddcd652d84d840171e72a4ed23bb5127
EppuHeilimo <eppu.heilimo@yahoo.com>
2017-06-13 10:24:06 +0300

    translate comments to english

--- opencv_install.sh
+++ opencv_install.sh
@@ -1,8 +1,5 @@
-#Asentaa perus kehitystyökalut
 sudo apt-get install build-essential cmake pkg-config -y
 
-
-#Asentaa I/O paketit, millä voidaan käyttää eri formaatteja
 sudo apt-get install libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev -y
 
 #GUI build
@@ -14,26 +11,23 @@ sudo apt-get install libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev -y
 sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev-y
 
 
-#"Optimointikirjastot"
+#"Optimization"
 sudo apt-get install libatlas-base-dev gfortran -y
 
-#Pip ja pip3
 sudo apt install python-pip python3-pip -y
 
-#Python 2 ja 3 dev tools
 sudo apt install python-dev python3-dev -y
 
 sudo apt install -y python3-tk
 sudo apt install -y python-tk
 
 
-#Asentaa numpy pythonille ja python3
 pip install numpy
 pip3 install numpy
 
 
 
-#Lataa OpenCV 3.2.0, pura se, rakenna (build), 
+#Install OpenCV 3.2.0, unzip, build, 
 wget -O opencv-3.2.0.zip https://sourceforge.net/projects/opencvlibrary/files/opencv-unix/3.2.0/opencv-3.2.0.zip/download
 unzip opencv-3.2.0.zip
 
@@ -41,10 +35,10 @@ cd opencv-3.2.0
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D BUILD_NEW_PYTHON_SUPPORT=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON  -D BUILD_EXAMPLES=ON ..
-#"Kasaa" (Compile)
+#Compile
 make
 
-#Asentaa OpenCV
+#Install OpenCV
 sudo make install
 sudo ldconfig
 
